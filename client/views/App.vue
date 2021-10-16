dc1ba8e8583459905a4ec27473860e68604f77ca
natewerm2000 <alexandn7@newpaltz.edu>
2021-10-15 22:54:06 -0400

    fixed

---
+++ client/views/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <div class="column">
+      <div class="column">
+        
+      </div>
+    </div>
+    <Nav />
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+
+</style>
+
+<script> 
+import Nav from "./components/Nav";
+
+export default {
+  components: {
+    Nav
+  }
+} 
+</script>
