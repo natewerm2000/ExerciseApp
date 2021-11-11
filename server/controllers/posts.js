8464092da91f46d383ca6d6e02723c9de20a8701
natewerm2000 <alexandn7@newpaltz.edu>
2021-11-11 12:46:07 -0500

    MongoDB Added

---
+++ server/controllers/posts.js
@@ -0,0 +1,27 @@
+/*  B"H
+*/
+const express = require("express");
+const model = require("../models/posts");
+
+const app = express.Router();
+
+app
+    .get("/", (req, res, next) =>{
+        res.send(model.GetAll());
+    })
+    .get("/search", (req, res, next) =>{
+        res.send(model.Search(req.query.q));
+    })
+    .get("/:id", (req, res, next) =>{
+        console.log(req.headers);
+
+        res.send(model.Get(req.params.id));
+    })
+    .post("/", (req, res, next) =>{
+        const newPost = model.Add(req.body)
+
+        res.status(201).send(newPost);
+    })
+
+
+module.exports = app;
--- server/index.js
+++ server/index.js
@@ -1,14 +1,33 @@
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
 
-const express = require('express')
+console.log(`The best class at New Paltz is ${process.env.BEST_CLASS}`);
+
+const usersController = require('./controllers/users');
+const postsController = require('./controllers/posts');
+
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-.get('/newpaltz', (req, res)=> {
-  res.send('hello New Paltz')
-})
+app
+    .use('/', express.static(path.join(__dirname, '../docs')) )
+
+    .use(express.json())
+    .use('/users', usersController )
+    .use('/posts', postsController)
+
+app
+    .get('*', (req, res) => res.sendFile(path.join(__dirname, '../docs/index.html')) )
+
+app
+    .use((err, req, res, next)=>{
+        // Besides for sending the error to the client. It is helpful to write it to the terminal/console.
+        // More information can be serialized to the console than can be serialized to JSON for transfer over the wire.
+        // Eventually you may want to add logic to prevent sensitive information from being sent to the client and to reformat the error message to make it more user friendly
+        console.error(err);
+        res.status(err.code || 500).send(err);
+    })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
